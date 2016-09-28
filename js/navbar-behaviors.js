a6da46f45a070e1a564f64f84eb535d34c5ebb0b
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-09-28 16:53:31 +0200

    added function - navbar doesn't cover content of sections (in progress)

--- js/navbar-behaviors.js
+++ js/navbar-behaviors.js
@@ -1,8 +1,25 @@
 /**
  * Created by agatakulbicka on 28.09.16.
  */
+
+//hides mobile menu after click on any element
+// Navbar doesn't hide page content
 $('.nav a').on('click', function(){
-    $('.btn-navbar').click(); 
+    $('.btn-navbar').click();
     $('.navbar-toggle').click()
 });
 
+// -48 - height of navbar -2 because of right scrollpsy
+//Navbar doesn't cover the content of sections after clicking in menu
+ var uncoveringContentUnderNavbar = function() {
+     scrollBy(0, -46)};
+ if (location.hash) {
+     uncoveringContentUnderNavbar();
+}
+ window.addEventListener("hashchange", uncoveringContentUnderNavbar);
+
+// $("nav").resize(function () {
+//     $('#godown').height($("nav").height() + 10);
+// });
+//
+// if ($("nav").height() > $('#godown').height()) $('#godown').height($("nav").height() + 10);
