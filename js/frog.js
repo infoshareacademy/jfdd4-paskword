68d46c7478bdd1d6f7e90878a8e8282f1d4f6e62
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-09-29 16:50:15 +0200

    added event handling to arrows keys

--- js/frog.js
+++ js/frog.js
@@ -2,3 +2,37 @@
  * Created by agatakulbicka on 29.09.16.
  */
 
+$(document).keydown(function (event) {
+
+    function left() {
+        console.log('1 lewo');
+    }
+
+    function up() {
+        console.log('2 góra');
+    }
+
+    function right() {
+        console.log('3 prawo');
+    }
+
+    function down() {
+        console.log('4 dół');
+    }
+
+    
+    switch (event.keyCode) {
+        case 37:
+            left();
+            break;
+        case 38:
+            up();
+            break;
+        case 39:
+            right();
+            break;
+        case 40:
+            down();
+            break;
+    }
+});
