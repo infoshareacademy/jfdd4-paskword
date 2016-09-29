6a9bf7261685d8ac4a46b28eea6054360b211bfb
Katarzyna Kaldunska <k.kaduska@gmail.com>
2016-09-29 10:53:14 +0200

    FDPF-7 add terms and conditions

---
+++ js/showTerms.js
@@ -0,0 +1,16 @@
+/**
+ * Created by kasiarzyna on 28.09.16.
+ */
+
+function showTerms() {
+    $('#hidden-terms').show();
+    $('html, body').animate({
+        scrollTop: $('#hidden-terms').offset().top
+    }, 2000);
+};
+
+$('#acceptTerms').change(function() {
+    if (this.checked) {
+        $('#hidden-terms').hide();
+    }
+});
