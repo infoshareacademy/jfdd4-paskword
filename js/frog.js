b87ae0a00d230cfcc6bcb40ea1bdd08afc756059
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-09-30 10:26:39 +0200

    added event to keys 'WSAD' and stopPropagination()

--- js/frog.js
+++ js/frog.js
@@ -1,38 +1,42 @@
 /**
  * Created by agatakulbicka on 29.09.16.
  */
+$(document).ready(function () {
+    $(document).keydown(function (event) {
 
-$(document).keydown(function (event) {
+        event.stopPropagation(); // arrow keys will be usee only for frog navigation -  arrows keydown won't move the site (up and down)
 
-    function left() {
-        console.log('1 lewo');
-    }
+        function left() {
+            console.log('1 lewo');
+        }
 
-    function up() {
-        console.log('2 góra');
-    }
+        function up() {
+            console.log('2 góra');
+        }
 
-    function right() {
-        console.log('3 prawo');
-    }
+        function right() {
+            console.log('3 prawo');
+        }
 
-    function down() {
-        console.log('4 dół');
-    }
+        function down() {
+            console.log('4 dół');
+        }
 
-    
-    switch (event.keyCode) {
-        case 37:
-            left();
-            break;
-        case 38:
-            up();
-            break;
-        case 39:
-            right();
-            break;
-        case 40:
-            down();
-            break;
-    }
+
+        //arrows and WSAD keydown action
+        switch (event.keyCode) {
+            case 37: case 65:
+                left();
+                break;
+            case 38: case 87:
+                up();
+                break;
+            case 39: case 68:
+                right();
+                break;
+            case 40: case 83:
+                down();
+                break;
+        }
+    });
 });
