8c48aebd3719e1594d0fea883ff22358e8439d86
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-10-03 12:17:32 +0200

    added frog moving functions (up, down, right, left)

--- js/frog.js
+++ js/frog.js
@@ -2,73 +2,90 @@
  * Created by agatakulbicka on 29.09.16.
  */
 $(document).ready(function () {
+    var score;
+    var croak = new Audio('sounds/frog.mp3');
+    var rowCount = $('#board tr').length;
+    var columnCount = $('tr td').length / rowCount;
+
+    var x = 0; //number of current column
+    var y = 0; // number of current row
+
+    //current location of the frog
+    document.getElementById(y + '-' + x).innerHTML = '<img src="images/small-frog.png">';
+
+    function removeOldFrog() {
+        document.getElementById(y + '-' + x).innerHTML = '';
+    }
+
+    function left() {
+        if (x > 0) {
+            removeOldFrog();
+            x -= 1;
+            document.getElementById(y + '-' + x).innerHTML = '<img src="images/small-frog.png">';
+            console.log(x);
+        }
+        else {
+            croak.play();
+        }
+    }
+
+    function up() {
+        if (y > 0) {
+            removeOldFrog();
+            y -= 1;
+            document.getElementById(y + '-' + x).innerHTML = '<img src="images/small-frog.png">';
+        }
+        else {
+            croak.play();
+        }
+    }
+
+
+    function right() {
+        if (x < (columnCount - 1)) {
+            removeOldFrog();
+            x += 1;
+            document.getElementById(y + '-' + x).innerHTML = '<img src="images/small-frog.png">';
+            console.log(x);
+        }
+        else {
+            croak.play();
+        }
+
+    }
+
+    function down() {
+        if (y < (rowCount - 2)) {
+            removeOldFrog();
+            y += 1;
+            document.getElementById(y + '-' + x).innerHTML = '<img src="images/small-frog.png">';
+        }
+        else {
+            console.log('brawo, wygrałeś!');
+        }
+    }
+
     $(document).keydown(function (event) {
-
-
-        $('#startLocation').removeClass('start-text').append('<img src="/images/frog-img.png">').css('z-index', 99999);
-
         event.stopPropagation(); // arrow keys will be usee only for frog navigation -  arrows keydown won't move the site (up and down)
-        var $gameboard = $('#nazwatabeli'); //dodać nazwę tabeli
-        var $occupatedCell = $('.occupatedCell');
-        var $indexOfCurrentLocation = $($occupatedCell).indexOf();
-        var $indexOfNewLocation;
-
-
-
-
-        function left() {
-            console.log('1 lewo');
-            $('$currentCell').closest('td').prev().find();
-        }
-        function up() {
-            $indexOfNewLocation = $occupatedCell.parent().closest('td').prev().find('td').indexOf($indexOfCurrentLocation);
-
-
-
-            //'pobierz pole', na które żabka chce przeskoczyć i sprawdź je:
-            // if(nie ma dokąd się ruszyć w górę)
-            // { nic nie rób}
-            //else
-            // { przesuń żabkę w górę
-            //odśwież planszę
-            //(if kolizja)
-            //      {
-            //         funkcja-smierc-zabki();
-            //          gameover
-            //         }
-            //    else {
-            //       zwiększ licznik punktów o jeden: licznik++;
-            // }
-            //
-            // }
-
-        }
-
-        function right() {
-            console.log('3 prawo');
-
-            $('$currentCell').closest('td').next().find();
-        }
-
-        function down() {
-            console.log('4 dół');
-        }
-
-
+        // event.preventDefault();
         //arrows and WSAD keydown action
         switch (event.keyCode) {
-            case 37: case 65:
-            left();
-            break;
-            case 38: case 87:
-            up();
-            break;
-            case 39: case 68:
-            right();
-            break;
-            case 40: case 83:
-            down();
-            break;
+            case 37:
+            case 65:
+                left();
+                break;
+            case 38:
+            case 87:
+                up();
+                break;
+            case 39:
+            case 68:
+                right();
+                break;
+            case 40:
+            case 83:
+                down();
+                break;
         }
     });
 });
