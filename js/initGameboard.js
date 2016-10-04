38dfff5c84978fbdcc255e02746f968cc4855cb2
Katarzyna Kałduńska <k.kaduska@gmail.com>
2016-10-05 00:06:28 +0200

    FDPF-39-street add riding trucks and street collision

--- js/initGameboard.js
+++ js/initGameboard.js
@@ -3,26 +3,146 @@
  *
  * Init Gameboard on click submit button
  * Gameboard is a table 12x12
+ * Init RidingTrucks on the streets
  */
+var rows = 12;
+var cols = 12;
+var speed = 500;
+var collision = false;
+var truckLoop;
 
-$(document).ready(function () {
-    $('#form1').on('submit', function (e) {
-        e.preventDefault();
+$(document).ready(function() {
+    $('#form1').on('submit', function(event) {
+        event.preventDefault();
         $('#form-submit').empty();
         initGameboard();
         initFrog();
         driftWood();
+        initIntervals();
     });
-
-
 });
+
+function initIntervals() {
+    makePattern(1);
+    makePattern(2);
+    makePattern(3);
+    makePattern(4);
+    truckLoop = setInterval(function() {
+
+        drawPattern(1, true);
+        drawPattern(2, true);
+        drawPattern(3, true);
+        drawPattern(4, false);
+
+    }, speed);
+}
+
+var patternArray = [];
+var patternArray2 = [];
+var patternArray3 = [];
+var patternArray4 = [];
+
+function makePattern(rowNum) {
+    var $truck = new Image(46,30);
+    var array = [];
+    $truck.src = "images/game-textures/truck.png";
+    $truck.alt = 'truck';
+    for (var j = 0; j < 100; j++) {
+        var distance = Math.round(Math.random() * 4 + 2);
+        array.push($truck);
+        for (distance; distance>0; distance--) {
+            array.push('');
+        }
+    }
+    if (rowNum == 1) {
+        patternArray = array;
+    } else if (rowNum == 2) {
+        patternArray2 = array;
+    } else if (rowNum == 3) {
+        patternArray3 = array;
+    } else if (rowNum == 4) {
+        patternArray4 = array;
+    }
+}
+
+function drawPattern(rowNum, mirror) {
+    var array = [];
+    if (rowNum == 1) {
+        array =  patternArray ;
+    } else if (rowNum == 2) {
+        array = patternArray2;
+    } else if (rowNum == 3) {
+        array = patternArray3;
+    } else if (rowNum == 4) {
+        array = patternArray4;
+    }
+    if (array.length === 0) {
+        makePattern(rowNum);
+    } else {
+
+        if (mirror) {
+            for (var k = 12; k > 0; k--) {
+                $('#' + rowNum + '-' + (k-1)).removeClass('showTruck mirror');
+                $('img[alt="truck"]').remove();
+            }
+            var targ = 1;
+            for (var i = 12; i > 0; i--) {
+                var $row = $('#' + rowNum + '-' + (i-1));
+                var lastEl = array[array.length-targ];
+                targ++;
+                if (lastEl != '') {
+                    $row.addClass('showTruck');
+                    var truck = lastEl;
+                } else {$row.append(lastEl);}
+
+            }
+            $('.showTruck').append(truck).addClass('mirror');
+
+            array.pop();
+        } else {
+            for (var k = 12; k > 0; k--) {
+                $('#' + rowNum + '-' + (k-1)).removeClass('showTruck mirror');
+                $('img[alt="truck"]').remove();
+            }
+            for (var i = 0; i < 12; i++) {
+                var $row = $('#' + rowNum + '-' + (i-1));
+                var lastEl = array[i];
+                if (lastEl != '') {
+                    $row.addClass('showTruck');
+                    var truck = lastEl;
+                } else {$row.append(lastEl);}
+
+            }
+            $('.showTruck').append(truck);
+
+            array.shift();
+        }
+    }
+
+    //Collision - frog dead
+    if ($('.showTruck').has('img[src="images/small-frog.png"]').length != 0
+        && !collision) {
+        console.log("KOLIZJA AAAAAAA");
+        collision = true;
+        console.log("przegryw");
+        clearInterval(truckLoop);
+    }
+
+    if (rowNum == 1) {
+        patternArray = array;
+    } else if (rowNum == 2) {
+        patternArray2 = array;
+    } else if (rowNum == 3) {
+        patternArray3 = array;
+    } else if (rowNum == 4) {
+        patternArray4 = array;
+    }
+}
 
 function initGameboard() {
     var gameBoard = $('<table>').attr('id', 'board').addClass('table');
     $('#form-submit').append(gameBoard);
-    var rows = 12;
-    var cols = 12;
-    // var imgFrog = "<img src='images/small-frog.png' />";
+
     for (var y = 0; y < cols; y++) {
 
         var $row = $('<tr>');
@@ -50,11 +170,8 @@ function initGameboard() {
                 $row.append($('<td>').attr('id', y + '-' + x).text('PUNKTY').addClass('col-xs-1 start-text points'));
             }
             else {
-                $row.append($('<td>').attr('id', y + '-' + x).text('x').addClass('col-xs-1'));
+                $row.append($('<td>').attr('id', y + '-' + x).addClass('col-xs-1'));
             }
         }
     }
 }
-
-// $('#form-submit').empty();
-// initGameboard();
