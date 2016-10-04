de442eeba1f360dcaa8cd9823c3aaa3ea175409e
Katarzyna Kaldunska <k.kaduska@gmail.com>
2016-10-04 09:15:22 +0200

    FDPF-39-street crazy trucks

--- js/initGameboard.js
+++ js/initGameboard.js
@@ -6,22 +6,27 @@
  */
 
 $(document).ready(function() {
-$('#form1').on('submit', function(e) {
-        e.preventDefault();
+    $('#form1').on('submit', function(event) {
+        event.preventDefault();
         $('#form-submit').empty();
         initGameboard();
         initFrog();
+        initRidingTrucks(1, true);
+        initRidingTrucks(2, false);
+        initRidingTrucks(3, true);
+        initRidingTrucks(4, false);
     });
 
 });
 
+var rows=11;
+var cols=11;
+
 function initGameboard() {
     var gameBoard = $('<table>').attr('id','board').addClass('table');
     $('#form-submit').append(gameBoard);
-    var rows=12;
-    var cols=12;
-    // var imgFrog = "<img src='images/small-frog.png' />";
-    for (var y=0; y<cols; y++) {
+
+    for (var y=0; y<=cols; y++) {
 
         var $row = $('<tr>');
 
@@ -37,7 +42,7 @@ function initGameboard() {
             $row.addClass('water');
             gameBoard.append($row);
         }
-        for (var x=0; x<rows; x++) {
+        for (var x=0; x<=rows; x++) {
             if (x==0 && y==0) {
                 $row.append($('<td>').attr('id', y + '-' + x).text('START').addClass('col-xs-1 start-text'));
             }
@@ -48,18 +53,59 @@ function initGameboard() {
                 $row.append($('<td>').attr('id', y + '-' + x).text('PUNKTY').addClass('col-xs-1 start-text points'));
             }
             else {
-                $row.append($('<td>').attr('id', y + '-' + x).text('x').addClass('col-xs-1'));
+                $row.append($('<td>').attr('id', y + '-' + x).addClass('col-xs-1'));
             }
         }
     }
-//Add trucks on the streets
-    var $truck = new Image(50,43);
-    $truck.src = "images/game-textures/truck.png";
-    var x = 1;
-    var y = 2;
-    setInterval(function() {
-        document.getElementById(y + '-' + x).append('$truck');
-        x++;
-    }, 300);
+}
 
+function initRidingTrucks(row,mirror) {
+    var $truck = new Image(50,36);
+    $truck.src = "images/game-textures/truck.png";
+    var speed = 600;
+    var x;
+    if (mirror) {
+        x = 0;
+    } else {
+        x = rows;
+    }
+
+    var z = 1;
+    console.log(z);
+    var always = false;
+    var truckLoop = setInterval(function() {
+
+        var truckNum = Math.floor(Math.random() * 3) + 1;
+        var truckDistance = Math.floor(Math.random() * 4) + 2;
+
+        if (mirror) {
+            $('#' + row + '-' + x).empty().append($truck).addClass('mirror');
+            x++;
+            console.log(x,rows);
+            if (x == rows+2) {
+                var xx = x-2;
+                $('#' + row + '-' + xx).empty();
+                clearInterval(truckLoop);
+                always = true ;
+            }
+        } else {
+            $('#' + row + '-' + x).empty().append($truck);
+            x--;
+            if (x == -2) {
+                var xx = x+2;
+                $('#' + row + '-' + xx).empty();
+                clearInterval(truckLoop);
+                always = true ;
+            }
+        }
+        //if ($('#' + row + '-' + x).hasClass( "frog" ).length) {
+        //katastrofa!
+        //}
+
+        if ((x == truckDistance && truckNum > z  && $('#' + row + '-' + x).children().length==0) || always) {
+            //console.log('puu');
+            z++;
+            initRidingTrucks(row,mirror);
+        }
+    }, speed);
 }
