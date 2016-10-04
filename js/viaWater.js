41e159befbf05f2c0889cc1056ac673efab96073
Agata Kulbicka <agata_kulbicka@wp.pl>
2016-10-04 21:12:52 +0200

    woods are moving  in two directions

--- js/viaWater.js
+++ js/viaWater.js
@@ -2,7 +2,7 @@
  * Created by agatakulbicka on 03.10.16.
  */
 
-var numberOfWoods = 4;
+var numberOfWoods = 5;
 var riverRows = 4;
 
 var woodsArray = [];
@@ -11,10 +11,8 @@ for (var j = 0; j < riverRows; j++) {
     var usedX = [];
     for (var i = 0; i < numberOfWoods; i++) {
         var x = randomNumber(0, 11);
-        // console.log('UsedX: ', usedX, x);
         while (usedX.indexOf(x) !== -1) {
             x = randomNumber(0, 11);
-            console.log('Jestem!', x);
         }
 
         usedX.push(x);
@@ -46,18 +44,32 @@ function startMoving() {
         removeOldWood(wood.x, wood.y);
     });
     woodsArray.forEach(function (wood) {
-        if (wood.x > 0) {
-            wood.x--;
-            addNewWood(wood.x, wood.y);
+        if (wood.y % 2 == 0) {
+            if (wood.x > 0) {
+                wood.x--;
+                addNewWood(wood.x, wood.y);
+            }
+            else {
+                wood.x = 11;
+                addNewWood(wood.x, wood.y);
+            }
         }
         else {
-            wood.x = 11;
-            addNewWood(wood.x, wood.y);
+            if (wood.x < 11) {
+                wood.x++;
+                addNewWood(wood.x, wood.y);
+            }
+            else {
+                wood.x = 0;
+                addNewWood(wood.x, wood.y);
+            }
         }
+
     });
+
 }
 
 function driftWood() {
-    setInterval(startMoving, 500);
+    setInterval(startMoving, 1000);
 }
 
